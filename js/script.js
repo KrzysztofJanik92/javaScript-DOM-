705cc75a0eae666750696f0bbb87ba8912cdf89e
KrzysztofJanik92 <krzysztof.janik@vp.pl>
2017-03-07 02:26:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 function zmien(fun) {
-   var html = document.getElementById('html');
-  if(fun.value==1) html.innerHTML = '<div class="text"><h2>The Shawshank Redemption</h2><img src="img/shawshank.jpg"><p >The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont, and starring Tim Robbins and Morgan Freeman. Adapted from the Stephen King novella Rita Hayworth and Shawshank Redemption, the film tells the story of Andy Dufresne, a banker who is sentenced to life in Shawshank State Penitentiary for the murder of his wife and her lover, despite his claims of innocence. During his time at the prison, he befriends a fellow inmate, Ellis Boyd "Red" Redding, and finds himself protected by the guards after the warden begins using him in his money-laundering operation.</p><p>The film received multiple award nominations (including seven Oscar nominations) and highly positive reviews from critics for its acting, story, and realism. Through Ted Turners acquisition of Castle Rock Entertainment, the film started gaining more popularity in 1997 after it started near-daily airings on Turners TNT network. It is now considered to be one of the greatest films of the 1990s</p></div>';
+   var html = document.getElementById('innerContent');
+   
+   if (fun.value == 1) {
+   var imgLink = "img/shawshank.jpg";
+   var topic = "The Shawshank Redemption";
+   var content = document.getElementById('shawshank').textContent;
+   }
+   
+   if (fun.value == 2) {
+   var imgLink = "img/greenMile.jpg";
+   var topic = "The Green Mile";
+   var content = document.getElementById('greenMile').textContent;
+   }
     
-    if(fun.value==2) html.innerHTML = '<div class="text"><h2>The Green Mile</h2><img src="img/greenMile.jpg"><p >The Green Mile is a 1999 American fantasy crime drama film written and directed by Frank Darabont and adapted from the 1996 Stephen King novel of the same name. The film is told in a flashback format and stars Tom Hanks as Paul Edgecomb and Michael Clarke Duncan as John Coffey with supporting roles by David Morse, Bonnie Hunt, and James Cromwell. The film also features Dabbs Greer, in his final film, as the old Paul Edgecomb. The film tells the story of Pauls life as a death row corrections officer during the Great Depression in the United States, and the supernatural events he witnessed.</p><p>The film was nominated for four Academy Awards: Best Picture, Best Supporting Actor for Michael Clarke Duncan, Best Sound, and Best Adapted Screenplay.</p></div>';
+   if (fun.value == 3) {
+   var imgLink = "img/mist.jpg";
+   var topic = "The Mist";
+   var content = document.getElementById('mist').textContent;
+   }
+   
+   innerContent.innerHTML = '<div class="text"><h2>' + topic +'</h2><img src="'+imgLink+ '"><p>' + content + '</p></div>';
     
-    if(fun.value==3) html.innerHTML = '<div class="text"><h2>The Mist</h2><img src="img/mist.jpg"><p >The Green Mile is a 1999 American fantasy crime drama film written and directed by Frank Darabont and adapted from the 1996 Stephen King novel of the same name. The film is told in a flashback format and stars Tom Hanks as Paul Edgecomb and Michael Clarke Duncan as John Coffey with supporting roles by David Morse, Bonnie Hunt, and James Cromwell. The film also features Dabbs Greer, in his final film, as the old Paul Edgecomb. The film tells the story of Pauls life as a death row corrections officer during the Great Depression in the United States, and the supernatural events he witnessed.</p><p>The director revised the ending of the film to be darker than the novellas ending, a change to which King was amenable. Darabont also sought unique creature designs to differentiate them from his creatures in past films.</p></div>';
 }
